f6fdc6ee3970a6a567d35822825d440ced5f16dd
chenjie <657499186@qq.com>
2024-09-27 09:59:03 +0800

    utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,23 +1,30 @@
 package utils
 
 import (
-	"github.com/mozillazg/go-pinyin"
+	"crypto/md5"
+	"fmt"
 )
 
-//获取汉字首字母
+// 获取汉字首字母
 func ChineseCharacterInitials(hans string) string {
 
 	args := pinyin.NewArgs()
 
 	rows := pinyin.Pinyin(hans, args)
 
-	strResult:=""
-	for i:=0;i<len(rows);i++{
-		if len(rows[i])!=0{
-			str:=rows[i][0]
-			pi:=str[0:1]
-			strResult+=pi
+	strResult := ""
+	for i := 0; i < len(rows); i++ {
+		if len(rows[i]) != 0 {
+			str := rows[i][0]
+			pi := str[0:1]
+			strResult += pi
 		}
 	}
 	return strResult
+}
+
+func MD5(s string) string {
+	data := []byte(s)
+	hashed := md5.Sum(data)
+	return fmt.Sprintf("%x", hashed)
 }
